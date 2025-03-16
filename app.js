ffd260da95d6119e490e102b2aae8a7ee1288706
Agnel Joshy <agneljoshy7@gmail.com>
2025-03-16 08:03:38 +0530

    V-3.1

--- app.js
+++ app.js
@@ -5,7 +5,24 @@ const loginCredentials = {
 const button = document.querySelector('button');
 const usernameInput = document.getElementById('usernameInput')
 const passwordInput = document.getElementById('passwordInput')
+const enterValidUsername = document.querySelector('.enter-valid-username');
+const statusCheck = document.querySelector('.status-div');
 
+
+usernameInput.addEventListener('input',function(){
+if (usernameInput.value !== loginCredentials.name){
+        enterValidUsername.innerText = 'Enter a valid username';
+        usernameInput.style.outline = "2px solid red";
+        statusCheck.innerHTML =  '<span class="invalid-style">*</span>';
+       
+        
+}
+else if(usernameInput.value === loginCredentials.name){
+    enterValidUsername.innerText = '';
+    usernameInput.style.outline ='2px solid black';
+    statusCheck.innerHTML= ' <span class="green-tick">&#10003</span>';
+   }
+})
 
 button.addEventListener('click',function(){
    const usernameValue =usernameInput.value;
